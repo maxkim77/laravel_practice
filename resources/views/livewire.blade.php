5550f17a9e35dd87d8855246b007828924355f88
maxkim77 <maxkim7723@gmail.com>
2024-05-08 07:14:54 +0900

    :sparkles: update

--- resources/views/livewire.blade.php
+++ resources/views/livewire.blade.php
@@ -4,11 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
+        <!-- vite: html을 css js와 연결할때 사용함 -->
         @vite('resources/css/app.css', 'resources/js/app.js')
-    @livewireStyles
+        <!-- 완전한 작동을 위해 필수적으로 추가해야하는 코드 -->
+        @livewireStyles
     </head>
     <body>        
+        <!-- component를 호출하기 위해 사용함 -->
         <livewire:livewire/> 
+        <!-- 필수 동작요소 -->
         @livewireScripts
     </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    
     server:{
         hmr: {  
             host: 'localhost',
